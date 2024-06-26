d35909fec91df8aed68351548e1133c4206d7bdf
AleCurzi <alexandreccurzi@gmail.com>
2024-06-26 18:05:26 -0300

    SignUpScreen

--- Trabalho-Final/screens/Login/SignUpScreen.tsx
+++ Trabalho-Final/screens/Login/SignUpScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import AuthContext from '../../service/AuthContext';
@@ -28,12 +28,17 @@ export const SignUpScreen: React.FC = () => {
 
   useEffect(() => {
     if (user) {
-      navigation.navigate('Main'); // Navegue para a tela principal após o cadastro bem-sucedido
+      navigation.navigate('Inicio');
     }
   }, [user, navigation]);
 
   return (
     <View style={styles.container}>
+      <Image
+        source={require('../../assets/twitter.png')}
+        style={styles.logo}
+      />
+      <Text style={styles.texticon}>Cadastre-se no Twitter</Text>
       {localError && <Text style={styles.error}>{localError}</Text>}
       {errorMessage && <Text style={styles.error}>{errorMessage}</Text>}
       <TextInput
@@ -41,12 +46,14 @@ export const SignUpScreen: React.FC = () => {
         placeholder="Nome"
         value={nome}
         onChangeText={setNome}
+        placeholderTextColor="#657786"
       />
       <TextInput
         style={styles.input}
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
+        placeholderTextColor="#657786"
       />
       <TextInput
         style={styles.input}
@@ -54,14 +61,21 @@ export const SignUpScreen: React.FC = () => {
         value={senha}
         onChangeText={setSenha}
         secureTextEntry
+        placeholderTextColor="#657786"
       />
       <TextInput
         style={styles.input}
         placeholder="Bio"
         value={bio}
         onChangeText={setBio}
+        placeholderTextColor="#657786"
       />
-      <Button title="Sign Up" onPress={handleSignUp} />
+      <TouchableOpacity style={styles.signUpButton} onPress={handleSignUp}>
+        <Text style={styles.buttonText}>Cadastre-se</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.buttonTextLogin}>Já tem uma conta?</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -70,17 +84,65 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
+    backgroundColor: 'white',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 32,
   },
   input: {
-    height: 40,
-    borderColor: 'gray',
+    width: '80%',
+    height: 50,
+    padding: 10,
+    borderColor: '#AAB8C2',
     borderWidth: 1,
+    borderRadius: 25,
     marginBottom: 12,
-    padding: 8,
+    paddingLeft: 20,
+    backgroundColor: '#E1E8ED',
+    fontSize: 16,
+  },
+  signUpButton: {
+    width: '80%',
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#1DA1F2',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
   error: {
-    color: 'red',
-    marginBottom: 12,
+    color: 'gray',
+    marginBottom: 22,
+    justifyContent: 'center',
   },
+  texticon:{
+    color: 'black',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 22,
+  },
+  loginButton: {
+    width: '80%',
+    height: 50,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonTextLogin: {
+    color: '#1DA1F2',
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
 });
+
+
+export default SignUpScreen;
